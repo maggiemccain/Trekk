91fc5770ee552a2d278214f7cb1bb314cce00109
Maggie McCain <maggie.mccain13@gmail.com>
2016-09-13 15:28:45 +1000

    js logic, scalability

--- app.js
+++ app.js
@@ -1,7 +1,133 @@
 console.log('is this working');
+var destinations = [{
+  name: 'Nashville, TN',
+  lat: 36.174465,
+  long: -86.767960,
+  city: true,
+  population: 678889,
+  temperature: true,
+  vibes: 'foodie, music, southern hospitality',
+  mustsee: 'Tootsies, Country Music Hall of Fame, Printer\'s Alley',
+}, {
+  name: 'Asheville, NC',
+  lat: 35.590489,
+  long: -82.560352,
+  city: false,
+  population: 87236,
+  temperature: false,
+  vibes: 'scenic, hipster',
+  mustsee: 'Blue Ridge Parkway, Biltmore Estate'
+}, {
+  name: 'Austin, TX',
+  lat: 30.2671500,
+  long: -97.7430600,
+  city: true,
+  population: 885400,
+  temperature: true,
+  vibes: 'hipster, foodie, music',
+  mustsee: ''
+}, {
+  name: 'Charleston, SC',
+  lat: 32.784618,
+  long: -79.940918,
+  city: false,
+  population: 127999,
+  temperature: false,
+  vibes: 'southern hospitality, beachy, shopping',
+  mustsee: 'The Battery, King Street, Sullivan\'s Island'
+}, {
+  name: 'New Orleans, LA',
+  lat: 29.951065,
+  long: -90.071533,
+  city: false,
+  population: 378715,
+  temperature: true,
+  vibes: 'historic, music, artsy',
+  mustsee: 'Bourbon Street, Garden District'
+}, {
+  name: 'Savannah, GA',
+  lat: 32.076176,
+  long: -81.088371,
+  city: false,
+  population: 142772,
+  temperature: false,
+  vibes: 'historic, scenic',
+  mustsee: 'Riverside'
+}, {
+  name: 'New York City',
+  lat: 40.730610,
+  long: -73.935242,
+  city: true,
+  population: 8406000,
+  temperature: false,
+  vibes: 'big city, museums and art, nightlife',
+  mustsee: 'The Met, SOHO, Central Park'
+}]
+
+var filteredLocations = destinations.slice();
+//TOOK OUT .hide() on map
+var $map = $('#map');
+var $filterDiv = $('#question-box').hide();
+
+var filterIndex = 0;
+
+var filters = [
+  {
+    keyword: 'city',
+    yes: 'https://newevolutiondesigns.com/images/freebies/city-wallpaper-47.jpg',
+    no: 'http://feelgrafix.com/data_images/out/5/779391-countryside.jpg'
+  },
+  {
+    keyword: 'temperature',
+    yes: 'http://static.asiawebdirect.com/m/phuket/portals/phuket-com/homepage/phuket-magazine/freedom-beach/pagePropertiesImage/freedom-beach.jpg',
+    no: 'http://www.powerfmballarat.com.au/images/snow.jpg'
+  }
+];
+
+
+function questionPrint() {
+  var $questionBox = $('#question-box');
+  var yesImage = $('<img>').attr('src', filters[filterIndex].yes);
+  var noImage = $('<img>').attr('src', filters[filterIndex].no);
+  var keyword = filters[filterIndex].keyword;
+  yesImage.click(function() {
+    filterLocation(keyword, true);
+    if (filters[filterIndex + 1]) {
+      filterIndex ++;
+      $questionBox.empty();
+      questionPrint();
+    } else {
+      $filterDiv.hide();
+      $map.show()
+    }
+    console.log(filteredLocations);
+  });
+
+  noImage.click(function() {
+    filterLocation(keyword, false);
+    if (filters[filterIndex + 1]) {
+      filterIndex ++;
+      $questionBox.empty();
+      questionPrint();
+    } else {
+      $filterDiv.hide();
+      $map.show();
+    }
+    console.log(filteredLocations);
+  });
+
+  $questionBox.append(yesImage);
+  $questionBox.append(noImage);
+}
+//FILTERING
+function filterLocation(keyword, bool) {
+  filteredLocations = filteredLocations.filter(function(element) {
+    return element[keyword] == bool;
+  })
+}
 
 $('#readyBtn').on('click', function(event){
   $(event.target).closest('div').hide();
-  $('#firstFilter').show();
-  console.log('clicked ready button');
+  $filterDiv.show();
+  questionPrint();
 })
--- destination.js
+++ destination.js
@@ -1,57 +1,66 @@
+//filter should be boolean
+
 var destinations = [{
   name: 'Nashville, TN',
   lat: 36.174465,
   long: -86.767960,
+  city: true,
   population: 678889,
-  temperature: '',
+  temperature: true,
   vibes: 'foodie, music, southern hospitality',
   mustsee: 'Tootsies, Country Music Hall of Fame, Printer\'s Alley',
 }, {
   name: 'Asheville, NC',
   lat: 35.590489,
   long: -82.560352,
+  city: false,
   population: 87236,
-  temperature: '',
+  temperature: false,
   vibes: 'scenic, hipster',
   mustsee: 'Blue Ridge Parkway, Biltmore Estate'
 }, {
   name: 'Austin, TX',
   lat: 30.2671500,
   long: -97.7430600,
+  city: true,
   population: 885400,
-  temperature: '',
+  temperature: true,
   vibes: 'hipster, foodie, music',
   mustsee: ''
 }, {
   name: 'Charleston, SC',
   lat: 32.784618,
   long: -79.940918,
+  city: false,
   population: 127999,
-  temperature: '',
+  temperature: false,
   vibes: 'southern hospitality, beachy, shopping',
   mustsee: 'The Battery, King Street, Sullivan\'s Island'
 }, {
   name: 'New Orleans, LA',
   lat: 29.951065,
   long: -90.071533,
+  city: false,
   population: 378715,
-  temperature: '',
+  temperature: true,
   vibes: 'historic, music, artsy',
   mustsee: 'Bourbon Street, Garden District'
 }, {
   name: 'Savannah, GA',
   lat: 32.076176,
   long: -81.088371,
+  city: false,
   population: 142772,
-  temperature: '',
+  temperature: false,
   vibes: 'historic, scenic',
   mustsee: 'Riverside'
 }, {
   name: 'New York City',
   lat: 40.730610,
   long: -73.935242,
+  city: true,
   population: 8406000,
-  temperature: '',
+  temperature: false,
   vibes: 'big city, museums and art, nightlife',
   mustsee: 'The Met, SOHO, Central Park'
 }]
